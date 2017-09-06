5d8291c0990cf97ac1c46439edfbbf5f8a016b9f
Thomas Kemmer <tkemmer@computer.org>
2017-09-06 18:48:01 +0200

    Fix #27: Update license headers.

--- src/hal/port_api.cpp
+++ src/hal/port_api.cpp
@@ -1,9 +1,6 @@
 /* mbino - basic mbed APIs for the Arduino platform
  * Copyright (c) 2017 Thomas Kemmer
  *
- * mbed Microcontroller Library
- * Copyright (c) 2006-2013 ARM Limited
- *
  * Licensed under the Apache License, Version 2.0 (the "License"); you
  * may not use this file except in compliance with the License.  You
  * may obtain a copy of the License at
