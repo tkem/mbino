f1bc1fdbd830f0e58128e72713ed213202ce41f0
Thomas Kemmer <tkemmer@computer.org>
2017-09-20 19:15:29 +0200

    Implement missing core util functions.

--- src/platform/mbed_critical.h
+++ src/platform/mbed_critical.h
@@ -19,13 +19,45 @@
 #ifndef MBINO_UTIL_CRITICAL_H
 #define MBINO_UTIL_CRITICAL_H
 
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+bool core_util_are_interrupts_enabled(void);
+
+bool core_util_is_isr_active(void);
+
 void core_util_critical_section_enter(void);
 
 void core_util_critical_section_exit(void);
+
+bool core_util_atomic_cas_u8(uint8_t *ptr, uint8_t *expectedCurrentValue, uint8_t desiredValue);
+
+bool core_util_atomic_cas_u16(uint16_t *ptr, uint16_t *expectedCurrentValue, uint16_t desiredValue);
+
+bool core_util_atomic_cas_u32(uint32_t *ptr, uint32_t *expectedCurrentValue, uint32_t desiredValue);
+
+bool core_util_atomic_cas_ptr(void **ptr, void **expectedCurrentValue, void *desiredValue);
+
+uint8_t core_util_atomic_incr_u8(uint8_t *valuePtr, uint8_t delta);
+
+uint16_t core_util_atomic_incr_u16(uint16_t *valuePtr, uint16_t delta);
+
+uint32_t core_util_atomic_incr_u32(uint32_t *valuePtr, uint32_t delta);
+
+void *core_util_atomic_incr_ptr(void **valuePtr, ptrdiff_t delta);
+
+uint8_t core_util_atomic_decr_u8(uint8_t *valuePtr, uint8_t delta);
+
+uint16_t core_util_atomic_decr_u16(uint16_t *valuePtr, uint16_t delta);
+
+uint32_t core_util_atomic_decr_u32(uint32_t *valuePtr, uint32_t delta);
+
+void *core_util_atomic_decr_ptr(void **valuePtr, ptrdiff_t delta);
 
 #ifdef __cplusplus
 } // extern "C"
